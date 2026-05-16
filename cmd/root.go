ad1c0fde6f869847bacb15b5ab0001a1c0cca2a8
Karim Mostafa <KarimMostafa842003@gmail.com>
2026-05-16 16:16:54 +0300

    chore: initialize prayer-cli project with Cobra CLI setup

---
+++ cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "pray",
+	Short: "A CLI tool that shows Islamic prayer times",
+	Long: "pray - Get Islamic prayer times based on your location, right in your terminal.",
+	
+	Run: func(cmd *cobra.Command, args []string){
+		fmt.Println("Use 'pray now', 'pray next', or 'pray today'")
+	},
+}
+
+func Execute(){
+	if err := rootCmd.Execute(); err != nil {
+        fmt.Fprintln(os.Stderr, err)
+        os.Exit(1)
+    }
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "prayer-cli/cmd"
+
+func main(){
+	cmd.Execute()
+}
