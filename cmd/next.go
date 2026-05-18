df2651696805f6cf20aca0deb80b3f3a2d6e0f4e
Karim Mostafa <KarimMostafa842003@gmail.com>
2026-05-18 13:36:20 +0300

    feat(cmd): add 'next' command to show upcoming prayer and time remaining

---
+++ cmd/next.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"prayer-cli/prayer"
+	"prayer-cli/utils"
+
+	"github.com/spf13/cobra"
+)
+
+var nextCmd = &cobra.Command{
+	Use:   "next",
+	Short: "Show the next prayer and time remaining",
+	Run: func(cmd *cobra.Command, args []string){
+		city, _ := cmd.Flags().GetString("city")
+		country, _ := cmd.Flags().GetString("country")
+
+		loc, err := resolveLocation(city, country)
+		if err != nil{
+			exit(err)
+		}
+
+		schedule, err := fetchSchedule(loc)
+		if err != nil{
+			exit(err)
+		}
+
+		_, next := prayer.GetCurrentAndNext(schedule)
+		remaining := ""
+		if next != nil{
+			remaining = prayer.TimeRemaining(next)
+		}
+
+		utils.PrintNext(getLocationString(loc), next, remaining)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(nextCmd)
+
+	nextCmd.Flags().StringP("city", "c", "", "City name (e.g. 'New York')")
+	nextCmd.Flags().StringP("country", "C", "", "Country name or code (e.g. 'US')")
+}
