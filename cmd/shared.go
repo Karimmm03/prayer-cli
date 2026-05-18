96439dd3157e59f3c703fe215d365acd24eea3b0
Karim Mostafa <KarimMostafa842003@gmail.com>
2026-05-18 13:27:22 +0300

    feat(cmd): add shared logic for CLI commands

---
+++ cmd/shared.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"prayer-cli/api"
+	"prayer-cli/location"
+	"prayer-cli/prayer"
+)
+
+func getLocationString(loc *location.Location) string{
+	if loc.Country != ""{
+		return fmt.Sprintf("%s, %s", loc.City, loc.Country)
+	}
+	return loc.City
+}
+
+func resolveLocation (city, country string) (*location.Location, error){
+	if city != ""{
+		return &location.Location{City: city, Country: country}, nil
+	}
+
+	loc, err := location.GetFromIP()
+	if err != nil{
+		return nil, fmt.Errorf("could not detect location: %w", err)
+	}
+	return loc, nil
+}
+
+func fetchSchedule (loc *location.Location) (*prayer.Schedule, error){
+	timings, err := api.GetPrayerTimes(loc.City, loc.Country)
+	if err != nil{
+		return nil, fmt.Errorf("could not fetch prayer times: %w", err)
+	}
+
+	schedule, err := prayer.BuildSchedule(timings)
+	if err != nil{
+		return nil, fmt.Errorf("could not build schedule: %w", err)
+	}
+
+	return schedule, nil
+}
+
+func exit(err error) {
+	fmt.Fprintf(os.Stderr, "  Error: %v\n\n", err)
+	os.Exit(1)
+}
